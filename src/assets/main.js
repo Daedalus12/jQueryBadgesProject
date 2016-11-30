c731d1afcc2a476ecd638054f3a5ea12733ca3b7
Greg Meess <greg.meess@autodesk.com>
2016-11-30 13:38:24 -0800

    Comlete challenge

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
-$(function() {
+$.ajax({
+  url: 'https://www.codeschool.com/users/1685053.json',
+  dataType: 'jsonp',
+  success: function(response)
+  {
+    for (i = 0; i < response.courses.completed.length; i++)
+    {
+      var course = response.courses.completed[i];
+      $("#badges").append(
+        "<div class='course'>" +
+        "<h3 class='title'>" + course.title + "</h3>" +
+        "<img src='" + course.badge + "'/>" +
+        "<a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a>" +
+        "</div>");
+    }
 
-  // your code will go here
+  }
 
 });
